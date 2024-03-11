bbeaa40fbe8fdd7d1a5573062a434dc0fb2c7377
kite-torder <jjy5@torder.com>
2024-03-11 15:21:51 +0900

    feat: 앱 테마 적용 (글꼴)

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,9 @@ import 'package:live_video_call/screen/home_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: "NotoSans"),
       home: HomeScreen(),
     ),
   );
