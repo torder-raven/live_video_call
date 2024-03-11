637292f90d9041850dba87184b0fffa5b82c12ed
kite-torder <jjy5@torder.com>
2024-03-11 15:20:26 +0900

    feat: 홈 스크린 설계

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:live_video_call/screen/home_screen.dart';
 
 void main() {
-  runApp(const MaterialApp());
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    ),
+  );
 }
